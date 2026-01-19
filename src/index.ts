4a3bc1b5f917b8d609d959fc9d7446391b37c820
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-01-19 15:40:42 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
+import { errorHandler } from "./middleware/error-handler.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Development Platforms CA API",
+      version: "1.0.0",
+      description: "API for managing users and news articles.",
+    },
+    servers: [{ url: `http://localhost:${PORT}` }],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello world!" });
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
